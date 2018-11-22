60a4511655d535ccf0ea29cb273e404319e5c3d2
SHERlocked93 <443033099@qq.com>
2018-11-22 21:16:25 +0800

    改一下id

--- .easy-mock.js
+++ .easy-mock.js
@@ -1,10 +1,10 @@
 module.exports = {
-  host: 'http://172.16.33.105:7300/',
+  // host: 'http://172.16.33.105:7300/',
   output: "../",
   template: "../",
   projects: [
     {
-      id: "5bf685b69254fa05b2386b8d",
+      id: "5bf6a23c92b5d9334494e884",
       name: "api-gene"
     }
   ]
