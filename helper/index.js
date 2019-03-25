ee917aa0ac76f7e5ac367ddc9f104f8510d69505
SHERlocked93 <443033099@qq.com>
2019-03-25 14:55:26 +0800

    fix: 增加对param为空的验证

--- helper/index.js
+++ helper/index.js
@@ -1,4 +1,4 @@
-var convertUrl = exports.convertUrl = function (url) {
+var convertUrl = exports.convertUrl = function(url) {
   // /restful/:id/:list/{id} -> restful_id_list_id
   // /restful/:id/:list/{id}.json -> restful_id_list_id
   var _url = url
@@ -6,31 +6,33 @@ var convertUrl = exports.convertUrl = function (url) {
     .replace(/-/g, '_')
     .split('/')
     .filter(value => !!value).join('_')
-    .replace(/_(\w)/g, (str, letter) => letter.toUpperCase());  // 改成小驼峰
-  return _url.split('.')[0];
-};
+    .replace(/_(\w)/g, (str, letter) => letter.toUpperCase())  // 改成小驼峰
+  return _url.split('.')[0]
+}
 
-exports.convertMethod = function (mock) {
+exports.convertMethod = function(mock) {
   // 防止重名
   // restful_id_list_id => restful_id_list_id_g
   // or
   // restful_id_list_id => restful_id_list_id_p
-  return convertUrl(mock.url);// + '_' + mock.method.toLowerCase();
-};
-
-exports.convertParam = function (mock) {
-  const params = JSON.parse(mock.parameters)
-  return params.map(T => T.name).join(', ');
-};
-
-exports.joinUrl = function () {
-  // https://www.easy-mock.com//mock/.... => https://www.easy-mock.com/mock/....
-  var url = [].slice.call(arguments, 0).join('/');
-  url = url.replace(/:\//g, '://');
-  url = url.replace(/([^:\s\%\3\A])\/+/g, '$1/');
-  return url;
+  return convertUrl(mock.url)// + '_' + mock.method.toLowerCase();
 }
 
-exports.isREST = function (url) {
-  return /(:|{|})/.test(url);
+exports.convertParam = function(mock) {
+  if (mock.parameters) {
+    const params = JSON.parse(mock.parameters)
+    return params.map(T => T.name).join(', ')
+  } else return ''
+}
+
+exports.joinUrl = function() {
+  // https://www.easy-mock.com//mock/.... => https://www.easy-mock.com/mock/....
+  var url = [].slice.call(arguments, 0).join('/')
+  url = url.replace(/:\//g, '://')
+  url = url.replace(/([^:\s\%\3\A])\/+/g, '$1/')
+  return url
+}
+
+exports.isREST = function(url) {
+  return /(:|{|})/.test(url)
 }
